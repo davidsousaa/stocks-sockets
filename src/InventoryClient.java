96c1e5edc12b4468b3e326de75a830d3e17939f9
davidsousaa <david.eira.sousa@gmail.com>
2023-10-12 23:46:10 +0100

    Fixed thread and menu

--- src/InventoryClient.java
+++ src/InventoryClient.java
@@ -9,9 +9,9 @@ public class InventoryClient {
 	public static void main(String[] args) {
 		String servidor=DEFAULT_HOST;
 		int port=DEFAULT_PORT;
-		String menu = " - Change Inventory - \n1 - Maça\n2 - Banana\n3 - Pera\n0 - Exit\n";
-		String itemsMenu = "1 - Maça\n2 - Banana\n3 - Pera\n Choose an option: ";
+		String menu = " - Change Inventory - \n1 - Maça\n2 - Banana\n3 - Pera\n0 - Exit\nChoose an option: ";
 		int option = -1;
+		int quantity = -1;
 		Scanner sc = new Scanner(System.in);
 		
 		if (args.length != 1) {
@@ -21,8 +21,6 @@ public class InventoryClient {
 
 		if (args.length >= 1) servidor = args[0];
 		if (args.length >= 2) port = Integer.parseInt(args[1]);
-	
-		// Create a representation of the IP address of the Server: API java.net.InetAddress
 
 		InetAddress serverAdress = null;
 
@@ -32,9 +30,6 @@ public class InventoryClient {
 			System.out.println("Erro ao obter o endereco do servidor: "+e1);
 			System.exit(1);
 		}
-
-		
-		// Create a client sockets (also called just "sockets"). A socket is an endpoint for communication between two machines: API java.net.Socket
 		
 		Socket client = null;
 
@@ -46,79 +41,53 @@ public class InventoryClient {
 		}
 
 		try {
-			
-			// Create a java.io.BufferedReader for the Socket; Use java.io.Socket.getInputStream() to obtain the Socket input stream
 				
-				BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			
-			// Create a java.io. PrintWriter for the Socket; Use java.io.Socket.etOutputStream() to obtain the Socket output stream
-	
-				PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+
 			String request = null;
 
 			ThreadRequest thread = new ThreadRequest();
 
 			do{
 				System.out.println(menu);
-				System.out.println("Choose an option: ");
 				sc = new Scanner(System.in);
 				if(sc.hasNextInt())
 				option = sc.nextInt();
+				sc.nextLine();
+				System.out.println("Quantity: ");
+				quantity = sc.nextInt();
 				sc.nextLine();
 			} while(option > 3 || option < 0);
 
 			switch(option){
 				case 1:
-					request = "STOCK_REQUEST";
+					request = "STOCK_UPDATE" + " " + "Maca" + " " + quantity;
 					break;
 				case 2:
-					do {
-						System.out.println(itemsMenu);
-						if(sc.hasNextInt())
-						option = sc.nextInt();
-						sc.nextLine();
-						System.out.println("Quantity: ");
-						int quantity = sc.nextInt();
-						sc.nextLine();
-						switch (option) {
-							case 1:
-								request = "STOCK_UPDATE" + " " + "Maca" + " " + quantity;
-								break;
-							case 2:
-								request = "STOCK_UPDATE" + " " + "Banana" + " " + quantity;
-								break;
-							case 3:
-								request = "STOCK_UPDATE" + " " + "Pera" + " " + quantity;
-								break;
-							default:
-								System.out.println("Invalid option!");
-								break;
-						}
-					} while(option > 3 || option < 1);
-					
+					request = "STOCK_UPDATE" + " " + "Banana" + " " + quantity;
 					break;
-				case 0:
-					System.exit(0);
+				case 3:
+					request = "STOCK_UPDATE" + " " + "Pera" + " " + quantity;
+					break;
+				default:
+					System.out.println("Invalid option!");
 					break;
 			}
 
-			System.out.println("Request=" + request);
+			System.out.println("Request = " + request);
 
-			// write the request into the Socket
+			out.println(request);			
+			StringBuilder msg = new StringBuilder();
 			
-				out.println(request);			
-				StringBuilder msg = new StringBuilder();
-			
-			// Read the server response - read the data until null
 			int value = 0;
 			while ((value  = in.read()) != -1) {
                 msg.append((char) value);
             }
 
-			// inventory = inventory.fromString(msg.toString());
+			if(msg.toString().equals("STOCK_UPDATED")) thread.restart();
 
 			System.out.println("Response=" + msg.toString());
-		// Close the Socket
 			
 			sc.close();
 			client.close();
